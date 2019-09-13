085463fe91a2662927ae296a88d1a7f7ce008a77
martinperez2306 <perezmartincontacto@gmail.com>
2019-09-13 02:43:16 -0300

    Resolver DNS y devoler la primera IP o devolver custom domain

--- api/dominios.py
+++ api/dominios.py
@@ -1,57 +1,87 @@
+import dns.resolver
+
 from flask import abort, make_response
 
 # Data to serve with our API
+
+#Estructura de datos que representa los dominios
 dominios = {
-    'fi.uba.ar': {
-        'domain': 'fi.uba.ar',
-        'ip': '157.92.49.38',
+    'martin.domain': {
+        'domain': 'martin.domain',
+        'ip': '999.999.999.999',
         'custom': False
     },
 }
 
-# Create a handler for our read (GET) people
-def obtener():
+def resolveDns(domain):
     """
-    Esta funcion maneja el request GET /api/alumnos
+    Esta funcion resuelve por DNS las IPs del dominio
 
-    :return:        200 lista ordenada alfabeticamente de alumnos de la materia
+    :return:        lista de las IPs asociadas al dominio
     """
     # Create the list of people from our data
-    return sorted(dominios.values(), key=lambda dominio: dominio.get('nombre'))
+    ips = []
+    try:
+        # Obtiene las resoluciones dns para este dominio
+        result = dns.resolver.query(domain)
+        ips = [ip.address for ip in result]
+    except dns.resolver.NXDOMAIN as e:
+        # El dominio no existe, por lo que las resoluciones dns permanecen vacias
+        pass
+    except dns.resolver.NoAnswer as e:
+        # El resolver no responde, por lo que las resoluciones dns permanecen vacias
+        pass
+    return ips
 
-def obtener_uno(domain):
+def createDomain(domain,ip):
+    domain = {
+        'domain' : domain,
+        'ip' : ip,
+        'custom' : False
+    }
+    return domain
+
+def getDomain(domain):
     """
-    Esta funcion maneja el request GET /api/alumnos/{id_alumno}
+    Esta funcion maneja el request GET /api/domains/{domain}
 
-     :id_alumno body:  id del alumno que se quiere obtener
-    :return:        200 alumno, 404 alumno no encontrado
+     :domain body:  nombre del dominio el cual se resuelve el DNS para obtener su(s) IP(s) o bien se selecciona del sistema
+    :return:        200 Dominio, 404 Dominio no encontrado en sistema o no se encontro IP por DNS
     """
-    if domain not in dominios:
-        return abort(404, 'El Dominio solicitado no se encuentra en sistema')
+    ##PREGUNTA: ¿Que se debe hacer primero, resolver dns o encontrar los customs?
+    ##PREGUNTA: ¿Cacheamos los dominios del DNS y actualizamos el orden que regrese?
+    ##PREGUNTA: ¿Que pasa si agregamos un custom domain que coincida con uno de dns? -> Creo que esto no deberia permitirse en el POST
 
-    return dominios.get(domain)
+    ips = resolveDns(domain)
+    if not ips:
+        if domain not in dominios:
+            return abort(404, 'El Dominio solicitado no se encuentra en sistema o bien no se encontro IP por DNS')
+        else:
+            return dominios.get(domain)
+    else:
+        return createDomain(domain, ips[0])
 
-def crear(**kwargs):
+def addDomain(**kwargs):
     """
-    Esta funcion maneja el request POST /api/alumnos
+    Esta funcion maneja el request POST /api/custom-domains
 
-     :param body:  alumno a crear en la lista de alumnos
-    :return:        201 alumno creado, 400 dni o padron duplicado
+     :param body:  Dominio a crear en sistema
+    :return:        201 Dominio creado, 400 Dominio existente en sistema
     """
-    alumno = kwargs.get('body')
-    dni = alumno.get('dni')
-    padron = alumno.get('padron')
-    nombre = alumno.get('nombre')
-    if not dni or not padron or not nombre:
-        return abort(400, 'Faltan datos para crear un alumno')
+    dominio = kwargs.get('body')
+    domain = dominio.get('domain')
+    ip = dominio.get('ip')
+    custom = True
+    if not domain or not ip:
+        return abort(400, 'Faltan datos para crear un Dominio')
 
     dup = False
-    for alumno_existente in alumnos.values():
-        dup = dni == alumno_existente.get('dni') or padron == alumno_existente.get('padron')
+    for dominio_existente in dominios.values():
+        dup = domain == dominio_existente.get('domain')
         if dup: break
 
     if dup:
-        return abort(400, 'DNI o Padron ya existentes')
+        return abort(400, 'Dominio ya existente')
 
     new_id = max(alumnos.keys()) + 1
     alumno['id'] = new_id
